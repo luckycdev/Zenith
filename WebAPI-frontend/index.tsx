1ac969a4a53ad52a30878de412f34bafd634e48b
Vincent <luckzstuff@gmail.com>
2025-06-13 16:43:45 -0500

    remove unused webapi and updater

--- WebAPI-frontend/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './src/scripts/app';
-
-import 'bootstrap/dist/css/bootstrap.css';
-
-ReactDOM.render(<App />, document.getElementById('app'));
--- WebAPI-frontend/src/scripts/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ModVersions } from './mod-versions';
-export { default as ModVersion } from './mod-version';
